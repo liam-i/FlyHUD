685c0e8ba08d85133b4d500975b554e860e3218b
leo-lp <lipengmjy@163.com>
2020-03-13 19:26:26 +0800

    update files.
    Former-commit-id: 1f019f7fba88b04a5b607af04fab88f20a9d3efd

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "LPProgressHUD", targets: ["LPProgressHUD"]),
     ],
     targets: [
-        .target(name: "LPProgressHUD", path: "Source"),
+        .target(name: "LPProgressHUD", path: "Sources"),
     ],
     swiftLanguageVersions: [.v5]
 )
