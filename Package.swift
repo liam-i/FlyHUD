2114b4cd12deba33a2f5f2f5826747dba7b40703
leo-lp <lipengmjy@163.com>
2020-03-13 19:09:51 +0800

    Add Package.swift
    Former-commit-id: 8cb632f88f1ea94469df85ea5de0a757d56a18da

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LPProgressHUD",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "LPProgressHUD", targets: ["LPProgressHUD"]),
+    ],
+    targets: [
+        .target(name: "LPProgressHUD", path: "Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
